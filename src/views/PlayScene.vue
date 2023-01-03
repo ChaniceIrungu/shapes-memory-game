576f0db218d43f788b6113802d473eeb53205093
Chanice Irungu <chaniceashley@gmail.com>
2023-01-03 15:32:34 +0300

    comments on template

--- src/views/PlayScene.vue
+++ src/views/PlayScene.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="">
     <!-- display the timer and progress bar -->
-    <!-- <div id="timer" class="pt-2 font-bold text-base">
-      {{ timeLeft }} seconds remaining
-    </div> -->
     <div class="mt-2" id="progress" :style="{ width: progressWidth }"></div>
+
+    <!-- Display Health -->
     <div class="font-bold text-base flex pl-4">
       <span class="pr-4">Health :</span>
       <span v-for="heart in hearts" :key="heart">
         <img class="h-6 w-6" src="../assets/heart_icon.png" />
       </span>
     </div>
+
+    <!-- Score -->
     <div class="font-bold text-base pl-4">score : {{ this.score }}</div>
+
     <!-- display the game board -->
     <div class="pb-12 pt-4">
       <h3
@@ -21,12 +23,14 @@
       ></h3>
     </div>
 
+    <!-- Shapes go here -->
     <div
       id="shape-container"
       ref="shapeContainer"
       class="flex justify-center"
     ></div>
-    <!-- show true button first -->
+
+    <!-- show true button on the right -->
     <div v-show="showTrueButtonFirst" class="flex justify-between px-4 mt-32">
       <!-- false button left -->
       <div class="h-24 w-24">
@@ -46,6 +50,8 @@
         />
       </div>
     </div>
+
+    <!-- show true button on the left -->
     <div v-show="!showTrueButtonFirst" class="flex justify-between px-4 mt-32">
       <!-- true button on left side -->
       <div class="h-24 w-24">
@@ -94,7 +100,6 @@ export default {
       ],
 
       currentstatement: null,
-      // statementTemplate: null,
       statementShape: null,
       statementColor: null,
 
@@ -117,24 +122,66 @@ export default {
   },
 
   methods: {
-    // method to start the timer
-    startTimer() {
-      this.timer = setInterval(() => {
-        this.timeLeft--; // decrease the time left by 1 second
+    // method to create the shapes
+    createShapes() {
+      // randomize shape creation
+      const shape = this.shapes[Math.floor(Math.random() * this.shapes.length)];
+      this.currentShape = shape;
 
-        // update the progress bar
-        this.progressWidth = (this.timeLeft / this.duration) * 100 + "%";
+      // randomize color creation
+      const color = this.colors[Math.floor(Math.random() * this.colors.length)];
+      this.currentColor = color;
 
-        if (this.timeLeft <= 0) {
-          clearInterval(this.timer); // stop the timer when it reaches 0
-          this.buttonClickCounter = 0;
-          localStorage.setItem("score", this.score);
-          localStorage.setItem("correct", this.correct);
-          localStorage.setItem("wrong", this.correct);
-          this.goToOtherPage("time-up"); // display a message when the timer ends
-        }
-      }, 1000); // run the timer every 1000 milliseconds (1 second)
+      if (shape == "circle") {
+        this.$refs[this.shapeContainerRef].innerHTML = `
+        <svg width="177" height="177" viewBox="0 0 177 177" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="88.5" cy="88.5" r="81.5" stroke="${color}" stroke-width="14"/>
+          </svg>`;
+      } else {
+        this.$refs[this.shapeContainerRef].innerHTML = `
+        <svg width="177" height="177" viewBox="0 0 177 177" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="7" y="7" width="163" height="163" stroke="${color}" stroke-width="14"/>
+            </svg>`;
+      }
+      // create statement when shape is created
+      this.createStatement();
     },
+
+    createStatement() {
+      // Randomly select a shape and color that match the current shape and color
+      // with a probability of 1/3
+      let shape;
+      let color;
+      if (Math.random() < 1 / 4) {
+        shape = this.currentShape;
+        color = this.currentColor;
+      } else {
+        // Otherwise, randomly select a shape and color as before
+        shape = this.shapes[Math.floor(Math.random() * this.shapes.length)];
+        color = this.colors[Math.floor(Math.random() * this.colors.length)];
+      }
+      this.statementShape = shape;
+      this.statementColor = color;
+      let statementTemplate =
+        this.statementTemplates[
+          Math.floor(Math.random() * this.statementTemplates.length)
+        ];
+
+      if (/not/i.test(statementTemplate)) {
+        // if -ve
+        this.$refs[
+          this.statementContainerRef
+        ].innerHTML = `It's not a ${color} ${shape}`;
+        this.currentstatement = statementTemplate;
+      } else {
+        //if +ve
+        this.$refs[
+          this.statementContainerRef
+        ].innerHTML = `It's a ${color} ${shape}`;
+        this.currentstatement = statementTemplate;
+      }
+    },
+
     // method to handle checking if the selected shape
     checkAnswer(response) {
       // Check whether the user tapped true or false
@@ -201,6 +248,25 @@ export default {
       this.createShapes();
     },
 
+    // method to start the timer
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.timeLeft--; // decrease the time left by 1 second
+
+        // update the progress bar
+        this.progressWidth = (this.timeLeft / this.duration) * 100 + "%";
+
+        if (this.timeLeft <= 0) {
+          clearInterval(this.timer); // stop the timer when it reaches 0
+          this.buttonClickCounter = 0;
+          localStorage.setItem("score", this.score);
+          localStorage.setItem("correct", this.correct);
+          localStorage.setItem("wrong", this.correct);
+          this.goToOtherPage("time-up"); // display a message when the timer ends
+        }
+      }, 1000); // run the timer every 1000 milliseconds (1 second)
+    },
+
     updateScore(result) {
       if (result === "right") {
         this.score += 50;
@@ -224,72 +290,13 @@ export default {
         this.goToOtherPage("game-over");
       }
     },
+
     interchangeButtons() {
       // select the false and true buttons
       if (this.buttonClickCounter % 3 === 0) {
         this.showTrueButtonFirst = false;
       } else if (this.buttonClickCounter % 2 === 0) {
         this.showTrueButtonFirst = true;
-      }
-    },
-
-    // method to create the shapes
-    createShapes() {
-      // randomize shape creation
-      const shape = this.shapes[Math.floor(Math.random() * this.shapes.length)];
-      this.currentShape = shape;
-
-      // randomize color creation
-      const color = this.colors[Math.floor(Math.random() * this.colors.length)];
-      this.currentColor = color;
-
-      if (shape == "circle") {
-        this.$refs[this.shapeContainerRef].innerHTML = `
-        <svg width="177" height="177" viewBox="0 0 177 177" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="88.5" cy="88.5" r="81.5" stroke="${color}" stroke-width="14"/>
-          </svg>`;
-      } else {
-        this.$refs[this.shapeContainerRef].innerHTML = `
-        <svg width="177" height="177" viewBox="0 0 177 177" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="7" y="7" width="163" height="163" stroke="${color}" stroke-width="14"/>
-            </svg>`;
-      }
-      // create statement when shape is created
-      this.createStatement();
-    },
-
-    createStatement() {
-      // Randomly select a shape and color that match the current shape and color
-      // with a probability of 1/3
-      let shape;
-      let color;
-      if (Math.random() < 1 / 4) {
-        shape = this.currentShape;
-        color = this.currentColor;
-      } else {
-        // Otherwise, randomly select a shape and color as before
-        shape = this.shapes[Math.floor(Math.random() * this.shapes.length)];
-        color = this.colors[Math.floor(Math.random() * this.colors.length)];
-      }
-      this.statementShape = shape;
-      this.statementColor = color;
-      let statementTemplate =
-        this.statementTemplates[
-          Math.floor(Math.random() * this.statementTemplates.length)
-        ];
-
-      if (/not/i.test(statementTemplate)) {
-        // if -ve
-        this.$refs[
-          this.statementContainerRef
-        ].innerHTML = `It's not a ${color} ${shape}`;
-        this.currentstatement = statementTemplate;
-      } else {
-        //if +ve
-        this.$refs[
-          this.statementContainerRef
-        ].innerHTML = `It's a ${color} ${shape}`;
-        this.currentstatement = statementTemplate;
       }
     },
 
